3d8381296c1bad5136cfc3adb3660484f6be4ff1
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-05-07 13:29:19 -0300

    feat: Criando API com a Listagem de heróis

--- spiderverse/eslint.config.mjs
+++ spiderverse/eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -10,7 +11,8 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.plugins("unused-imports", "import"),
 ];
 
 export default eslintConfig;
--- spiderverse/next.config.ts
+++ spiderverse/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    API_URL: "https://681b86ad17018fe5057be851.mockapi.io",
+  },
 };
 
 export default nextConfig;
