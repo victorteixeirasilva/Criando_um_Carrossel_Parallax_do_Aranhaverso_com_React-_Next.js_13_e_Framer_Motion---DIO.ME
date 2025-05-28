4a05fad65828d8674276634949cc94968907e5fc
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-05-28 11:59:16 -0300

    Listagem de imagens dos heróis

--- spiderverse/src/components/HeroesList/index.tsx
+++ spiderverse/src/components/HeroesList/index.tsx
@@ -1,6 +1,7 @@
 import { spidermanFont } from "@/fonts";
 import { IHeroData } from "@/interfaces/heroes";
 import styles from "./heroesList.module.scss"
+import HeroesPicture from "../HeroesPicture";
 
 interface IProps { 
     heroes: IHeroData[]; // Replace 'any' with the actual type of your hero objects
@@ -11,7 +12,13 @@ export default function HeroesList({ heroes }: IProps) {
     return (
         <>
             <h1 className={`${spidermanFont.className} ${styles.title}`}>Personagens</h1>
-            
+            <section className={styles.heroes}>
+                {heroes.map((hero) => (
+                    <div key={hero.id} className={`${styles.imageContainer} ${styles[hero.id]}`}>
+                        <HeroesPicture hero={hero} />
+                    </div>
+                ) )}
+            </section>
         </>
     );
 }
