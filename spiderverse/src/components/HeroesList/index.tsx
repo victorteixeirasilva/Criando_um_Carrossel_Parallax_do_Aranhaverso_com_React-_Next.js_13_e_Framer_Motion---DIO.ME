d75ad6a8711dad9272bac2f27b760acb4bfdbb16
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-05-28 12:41:03 -0300

    Animando os elementos da página inicial

--- spiderverse/src/components/HeroesList/index.tsx
+++ spiderverse/src/components/HeroesList/index.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { spidermanFont } from "@/fonts";
 import { IHeroData } from "@/interfaces/heroes";
 import styles from "./heroesList.module.scss"
 import HeroesPicture from "../HeroesPicture";
+import { motion } from "framer-motion";
 
 interface IProps { 
     heroes: IHeroData[]; // Replace 'any' with the actual type of your hero objects
@@ -11,14 +14,26 @@ interface IProps {
 export default function HeroesList({ heroes }: IProps) { 
     return (
         <>
-            <h1 className={`${spidermanFont.className} ${styles.title}`}>Personagens</h1>
-            <section className={styles.heroes}>
+            <motion.h1 
+                className={`${spidermanFont.className} ${styles.title}`} 
+                initial={{ opacity: 0}}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 2, delay: 2 }}
+            >
+                Personagens
+            </motion.h1>
+            <motion.section 
+                className={styles.heroes}
+                initial={{ opacity: 0, y: -100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 2}}
+            >
                 {heroes.map((hero) => (
                     <div key={hero.id} className={`${styles.imageContainer} ${styles[hero.id]}`}>
                         <HeroesPicture hero={hero} />
                     </div>
                 ) )}
-            </section>
+            </motion.section>
         </>
     );
 }
